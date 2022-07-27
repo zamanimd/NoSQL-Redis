215192588ac498fcce48081a61882890da72cca4
Mohammad Zamani <45899172+zamanimd@users.noreply.github.com>
2022-07-27 15:18:19 -0600

    First Commit

---
+++ RedisDB/Program.cs
@@ -0,0 +1,18 @@
+﻿using ServiceStack.Redis;
+using RedisDB;
+
+var person = new Person
+{
+    FirstName = "Mohammad",
+    LastName = "Zamani"
+};
+
+var myID = Guid.NewGuid().ToString();
+
+using (var client = new RedisClient("localhost", 6379))
+{
+    client.Set<Person>(myID, person);
+}
+
+
+Console.WriteLine("Added item in redis database with this key: " + myID);
